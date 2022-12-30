a42f2c3fe92c5d9a01eca9f00028c05f9101653b
Elena Markova <webkwondo.com@gmail.com>
2022-12-30 03:52:14 +0300

    feat: add solutions to promises tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,14 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer !== 'boolean') reject(new Error('Wrong parameter is passed! Ask her again.'));
+
+    if (isPositiveAnswer) resolve('Hooray!!! She said "Yes"!');
+
+    resolve('Oh no, she said "No".');
+  });
 }
 
 
@@ -48,8 +54,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +77,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,8 +98,25 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  const results = [];
+  const errors = [];
+
+  const promiseChain = array.reduce((acc, curr) => acc.then((value) => {
+    results.push(value);
+    return curr;
+  }, (error) => {
+    errors.push(error);
+    return curr;
+  }));
+
+  return promiseChain.then((value) => {
+    results.push(value);
+    return results;
+  }, (error) => {
+    errors.push(error);
+    return results;
+  }).then((resultsArr) => resultsArr.reduce(action));
 }
 
 module.exports = {
